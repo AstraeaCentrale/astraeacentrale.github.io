c0f9c32655eafcc4609fbf263457e2c819e3333e
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-04-01 20:53:47 +0800

    to Astraea and beyond!
    「The Battle of Landosol has long ended, and Landosol is at peace. But the Gourmet Guild and Co. still ventures forth...」

--- evtcntdwn.js
+++ evtcntdwn.js
@@ -1,7 +1,7 @@
 //all code is based on W3Schools: https://www.w3schools.com/howto/howto_js_countdown.asp
 //and stackoverflow answer: https://stackoverflow.com/questions/40263537/javascript-countdown-timer-in-gmt-utc
 
-var countDownDate = new Date('Nov 22, 2022 23:00:00 GMT+00:00').getTime();
+var countDownDate = new Date('Apr 30, 2023 09:00:00 GMT+09:00').getTime();
 
 var x = setInterval(function() {
   
@@ -9,16 +9,20 @@ var x = setInterval(function() {
   
   var distance = countDownDate - now;
   
-  var days = ("0" + Math.floor(distance / (1000 * 60 * 60 * 24))).slice(-3);
+  var days = ("0" + Math.floor(distance / (1000 * 60 * 60 * 24))).slice(-2);
   var hours = ("0" + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
   var minutes = ("0" + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
   var seconds = ("0" + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
   
-  document.getElementById("evtTimer").innerHTML = days + ":" + hours + ":"
-  + minutes + ":" + seconds;
+  //document.getElementById("evtTimer").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+  document.title = days + ":" + hours + ":" + minutes + ":" + seconds;
   
-  if (distance < 0) {
+  if (distance <= 0) {
+
     clearInterval(x);
-    document.getElementById("evtTimer").innerHTML = "Event begins now!";
+
+    //document.getElementById("evtTimer").innerHTML = "Event begins now!";
+    document.title = "00:00:00:00";
+
   }
 }, 1000);
