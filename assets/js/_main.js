947515ae7379eff604486e7ad787553e7ccc07a6
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2025-06-21 12:31:53 +0800

    move `/senrimana` to its own repo
    https://github.com/AstraeaCentrale/senrimana

---
+++ assets/js/_main.js
@@ -0,0 +1,52 @@
+function start() {
+  inPageStart(); // per page start
+}
+
+function openletter() {
+
+  let stmt1 =
+  "1 of 2" +
+  "\n\n" +
+  "This Open Message has been released following the shutdown of Princess " +
+  "Connect! Re:Dive (Global EN) back in 30 Apr at 1am UTC." +
+  "\n\n" +
+  "It's been a long road that we took to get the players hyped up when " +
+  "it launched last 2020, and everything has paid off in the end. We " +
+  "would not be able to go this far were it not because of our dearest " +
+  "fans whom through this game has developed their interest in the " +
+  "anime as a whole." +
+  "\n\n" +
+  "And we would also thank the developers, maintainers and partners " +
+  "that make up the framework of the game for their unending love " +
+  "and passion that they poured to deliver the players the best " +
+  "experience that they could never get anywhere else." +
+  "\n\n" +
+  "As the sun sets around Landosol and the whole of Astraea, and as " +
+  "the servers are finally turned off for the last time, we are reminded " +
+  "that not all moments last forever. Sometimes, those that did good " +
+  "to us must go, leaving us only the memories that we shared." +
+  "\n\n" +
+  "We at GourmetDayLife / Astraea Centrale would like to thank all " +
+  "the developers and partners for making this dream possible, and all " +
+  "the players for connecting with each other through events, rankings, " +
+  "battles, story and so on.";
+
+  let stmt2 =
+  "2 of 2" +
+  "\n\n" +
+  "We would like to close this Open Message with a sentence that " +
+  "brought us to where we are today:" +
+  "\n" +
+  "「The Battle of Landosol has long ended, and Landosol is at peace. " +
+  "But the Gourmet Guild and Co. still ventures forth...」" +
+  "\n\n" +
+  "Until we meet again, Astraea." +
+  "\n\n" +
+  "thebelovedmoon" +
+  "\n" +
+  "GourmetDayLife / Astraea Centrale";
+
+  alert(stmt1);
+  alert(stmt2);
+
+}
